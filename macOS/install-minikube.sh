25a8edc718e255d807df34027de5ef3130339de3
Guanghua Shu <guanghuashu@gmail.com>
2019-12-24 21:26:39 -0800

    Initial commit: install minikube for macOS

---
+++ macOS/install-minikube.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Check whether virturalization is supported on macOS. Yes, if you see 'VMX' in colored output.
+sysctl -a | grep -E --color 'machdep.cpu.features|VMX'
+
+# Install hypervisor.
+# Any hypervisor (HyperKit, VirtualBox or VMWare Fusion) should do the work, HyperKit is a lightweight option for Mac.
+# If Docker for Desktop is installed, you already have hyperkit
+# Otherwise, brew it
+brew install hyperkit
+
+# If you want, you may build it from here:
+# https://github.com/moby/hyperkit
+
+
+# install minikube
+brew install minikube
+brew link hyperkit
+
+# Start minikube
+minikube start --vm-driver=hyperhit
+
+# Check status
+minikube status
+
+
+# Stop the cluster
+# minkube stop
